f2f4748b8129e4f3b9f3bfda44ac436a4a9226c9
Malte Schababerle <m.schababerle@gmail.com>
2020-02-22 13:14:56 +0100

    Fix tasks.json paths + some restructuring

--- src/extension.ts
+++ src/extension.ts
@@ -1,99 +1,105 @@
 import * as vscode from 'vscode';
-import * as fs from 'fs';
 
 let statusBarItems: Map<string, vscode.StatusBarItem> = new Map();
 let disposables: vscode.Disposable[] = [];
-let lastRead: Date = new Date(0);
-let tasksFilePath = '/Users/mschababerle/Documents/worspace/.vscode/tasks.json';
+let lastRead: number = 0;
+let context: vscode.ExtensionContext;
+let tasksPath: string = `${vscode.workspace.rootPath}/.vscode/tasks.json`;
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(this: any, con: vscode.ExtensionContext) {
+	context = con;
 	// wait for changes in tasks.json
-	let watcher = vscode.workspace.createFileSystemWatcher('**/tasks.json');
-
-	// on tasks.json changed
-	watcher.onDidChange((event) => {
-		fs.stat(tasksFilePath, function (err, stats) {
-
-			// workaround for didChange event fired twice for one change
-			let lastWrite = stats.mtime;
-			if (lastWrite.getTime() == lastRead.getTime()) {
-				return;
-			}
-			lastRead = lastWrite;
-			onTasksJsonChanged(context);
-		});
-	});
+	vscode.workspace.createFileSystemWatcher(tasksPath)
+		.onDidChange(onDidChangeTwiceWorkaound);
+	vscode.workspace.createFileSystemWatcher(tasksPath)
+		.onDidCreate(onDidChangeTwiceWorkaound);
+	vscode.workspace.createFileSystemWatcher(tasksPath)
+		.onDidDelete(cleanup);
 
 	// init status bar items
-	fs.stat(tasksFilePath, function (err, stats) {
-		if (err) {
-			console.error(err);
-			return;
-		}
-		onTasksJsonChanged(context);
-	});
+	onDidChangeTwiceWorkaound(vscode.Uri.file(tasksPath));
 }
 
-function onTasksJsonChanged(context: vscode.ExtensionContext) {
+async function onDidChangeTwiceWorkaound(tasksUri: vscode.Uri) {
+	try {
+		let stat = await vscode.workspace.fs.stat(tasksUri);
+		// workaround for didChange event fired twice for one change
+		let lastWrite = stat.mtime;
+		if (lastWrite === lastRead) {
+			return;
+		}
+		lastRead = lastWrite;
+	} catch (err) {
+		console.log("Can't open tasks.json: ", err);
+		return;
+	}
+
+	onTasksJsonChanged(tasksUri);
+}
+
+async function onTasksJsonChanged(tasksUri: vscode.Uri) {
 	// parse tasks.json file
-	let tasks = JSON.parse(fs.readFileSync(tasksFilePath).toString());
+	try {
+		let tasksFile = await vscode.workspace.fs.readFile(tasksUri);
+		let tasks = JSON.parse(tasksFile.toString().replace(/\s*\/\/.*\r?\n/g, ""));
 
-	// remove old statusBarItems and commands
-	cleanup();
+		// remove old statusBarItems and commands
+		cleanup();
 
-	// loop through input section of tasks.json
-	tasks.inputs.forEach((input: any) => {
+		// loop through input section of tasks.json
+		tasks.inputs.forEach((input: any) => {
 
-		// ignore inputs not intended for this extension
-		if (!input.command.startsWith('statusBarParam.getSelected.')) {
-			return;
-		}
-		addStatusBarParam(context, input.id, input.command, input.args);
-	}); // end loop inputs
+			// ignore inputs not intended for this extension
+			if (!input.command.startsWith('statusBarParam.getSelected.')) {
+				return;
+			}
+			addStatusBarParam(input.id, input.command, input.args);
+		}); // end loop inputs
+	} catch (err) {
+		console.log("Couldn't parse tasks.json", err);
+	}
 }
 
-function addStatusBarParam(context: vscode.ExtensionContext, id: string, command: string, selectables: string[]) {
+function addStatusBarParam(id: string, commandIDGetParam: string, selectables: string[]) {
 	// create command for selection of status bar param
-	let commandIDSelect = `statusBarParam.select.${id}`;
-	let commandSelectParam = vscode.commands.registerCommand(commandIDSelect, () => {
-		onStatusBarItemClick(selectables, statusBarItem)
-	});
-	context.subscriptions.push(commandSelectParam);
-	disposables.push(commandSelectParam);
+	let commandIDSelectParam = `statusBarParam.select.${id}`;
+	let commandIDPickParam = vscode.commands.registerCommand(commandIDSelectParam, () => onClick(selectables, statusBarItem));
+	context.subscriptions.push(commandIDPickParam);
+	disposables.push(commandIDPickParam);
 
 	// create status bar item
 	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	statusBarItems.set(command, statusBarItem);
-	statusBarItem.command = commandIDSelect;
+	statusBarItems.set(commandIDGetParam, statusBarItem);
+	statusBarItem.command = commandIDSelectParam;
 	statusBarItem.text = `Select \${input:${id}}...`;
 	context.subscriptions.push(statusBarItem);
 	disposables.push(statusBarItem);
-	
+
 	// return currently selected value of status bar param (when input:<input_id> is used in tasks.json)
-	let commandGetSelectedParam = vscode.commands.registerCommand(command, (...args: any[]): string | undefined => {
-		let statusBarItem = statusBarItems.get(command);
-		if (!statusBarItem) {
-			return undefined;
-		}
-		return statusBarItem.text;
-	});
-	context.subscriptions.push(commandGetSelectedParam);
-	disposables.push(commandGetSelectedParam);
-	
+	let commandGetParam = vscode.commands.registerCommand(commandIDGetParam, () => onGetParam(statusBarItem));
+	context.subscriptions.push(commandGetParam);
+	disposables.push(commandGetParam);
+
 	statusBarItem.show();
 }
 
-function onStatusBarItemClick(selectables: string[], statusBarItem: vscode.StatusBarItem) {
-	vscode.window.showQuickPick(selectables).then((value) => {
-		if (!value) {
-			return;
-		}
-		// set status bar item text on selected value
-		statusBarItem.text = value;
-	});
+async function onClick(selectables: string[], statusBarItem: vscode.StatusBarItem) {
+	let value = await vscode.window.showQuickPick(selectables);
+	if (!value) {
+		return;
+	}
+	// set status bar item text on selected value
+	statusBarItem.text = value;
 }
 
-function cleanup () {
+function onGetParam(statusBarItem: vscode.StatusBarItem) {
+	if (!statusBarItem) {
+		return undefined;
+	}
+	return statusBarItem.text;
+}
+
+function cleanup() {
 	disposables.forEach(disposable => {
 		disposable.dispose();
 	});
